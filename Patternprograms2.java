393937b795548c0212af3112f1de3d14d4c2ead2
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-01 23:33:58 +0530

    pattern printing

---
+++ Patternprograms2.java
@@ -0,0 +1,22 @@
+package datastructure;
+
+public class Patternprograms2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int row=3;row>=1;row--)
+		{
+			for(int col=1;col<=row;col++)
+			{
+				System.out.print(row);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
