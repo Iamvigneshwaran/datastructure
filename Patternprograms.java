e6fae78ad2aa679ec1c340aa7dd62fb98d9c05db
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-01 23:22:02 +0530

    pattern program

---
+++ Patternprograms.java
@@ -0,0 +1,24 @@
+package datastructure;
+
+public class Patternprograms {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		
+		for(int row=3;row>=1;row--)
+		{
+		           for(int col=3;col>=row;col--)
+		           {
+			                 System.out.print(col+" ");
+		            }
+		           
+		    System.out.println();
+		}
+		
+	}
+
+}
