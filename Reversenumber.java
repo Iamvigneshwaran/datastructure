ecebce4e3214f6aa42f1f6157b03f5860a8dab6a
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-03 23:20:51 +0530

    reverse a number program

--- Reversenumber.java
+++ Reversenumber.java
@@ -9,16 +9,17 @@ public class Reversenumber {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      int no,rem;
+      int no,rem=0,no2;
       System.out.println("enter the number:");
 		Scanner sc=new Scanner(System.in);
 		no=sc.nextInt();
 	
 		while(no>0){
-		rem=no%10;
-		System.out.print(rem);
+		rem=(no%10)+rem*10;
+		
 		no=no/10;
 		}
+		System.out.print(rem);
 	}
 
 }
