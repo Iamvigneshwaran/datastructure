abcd53ea799ae865a0e1685a98aa76f023a90124
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-04 22:36:49 +0530

    decimal to binary

---
+++ Decimal_to_binary.java
@@ -0,0 +1,26 @@
+package datastructure;
+
+import java.util.Scanner;
+public class Decimal_to_binary {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String binary="";
+		int rem,decimal;
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter the decimal number: ");
+		decimal=sc.nextInt();
+
+		while(decimal>0){
+		rem=decimal%2;
+		binary=rem+binary;
+		decimal=decimal/2;
+		}
+		System.out.println("binary number :"+binary);
+	}
+
+}
